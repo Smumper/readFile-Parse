64631ad26c32c4e568cecd7717195e7dcda1dc55
Peter Schwarz <p.schwarz1994@outlook.de>
2020-07-28 11:33:54 +0200

    Areas & Projekt infos klasse erstellt

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from readFile import readFile
 from parser.parseFile import parseFile
 from parser.parseExport import parseExport
+from projectinfo import Project_information
+from Area import Areas
+
 
 filepath = (
     "/Users/peterschwarz/VS Code Projekte/readFile-Parse/elektrolyse_20200326.csv"
--- projectinfo.py
+++ projectinfo.py
@@ -1,6 +1,25 @@
 class Project_information:
-    def __init__(self):
-        pass
+    def __init__(self, data):
+        self.data = data.split(";")
 
     def getName(self):
-        print("Test")
+        return self.data[1]
+
+    def getCommi(self):
+        return self.data[2]
+
+    def getCreator(self):
+        return self.data[3]
+
+    def getCompany(self):
+        return self.data[14]
+
+    def getVersionDate(self):
+        date = str(self.data[6] + "." + self.data[7] + "." + self.data[8])
+        time = str(self.data[9] + "." + self.data[10] + "." + self.data[11])
+        return date + ":" + time
+
+    def getCreationDate(self):
+        date = str(self.data[20] + "." + self.data[21] + "." + self.data[22])
+        time = str(self.data[23] + "." + self.data[24] + "." + self.data[25])
+        return date + ":" + time
